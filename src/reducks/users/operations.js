2fc099efbc3b19b1f70a9a68b2c6d54ff530a4e4
eito0201 <eito.k.8888@gmail.com>
2020-08-27 17:53:00 +0900

    add signUp operation

--- src/reducks/users/operations.js
+++ src/reducks/users/operations.js
@@ -1,5 +1,7 @@
 import { signInAction } from './actions';
 import { push } from 'connected-react-router';
+import { isValidEmailFormat, isValidRequiredInput } from '../../function/common';
+import { auth, FirebaseTimestamp, db } from '../../firebase/index';
 
 export const signIn = () => {
   return async (dispatch, getState) => {
@@ -23,5 +25,50 @@ export const signIn = () => {
 
       dispatch(push('/'));
     }
+  };
+};
+
+export const signUp = (username, email, password, confirmPassword) => {
+  return async dispatch => {
+    // バリデーションチェック
+    if (!isValidRequiredInput(username, email, password, confirmPassword)) {
+      alert('必須項目が未入力です。');
+      return false;
+    }
+    if (!isValidEmailFormat(email)) {
+      alert('メールアドレスの形式が不正です。もう1度お試しください。');
+    }
+    if (password !== confirmPassword) {
+      alert('パスワードが一致しません。もう1度お試しください。');
+      return false;
+    }
+    if (password.length < 6) {
+      alert('パスワードは6文字以上で入力してください。');
+      return false;
+    }
+
+    return auth.createUserWithEmailAndPassword(email, password).then(result => {
+      const user = result.user;
+
+      if (user) {
+        const uid = user.uid;
+        const timestamp = FirebaseTimestamp.now();
+        const userInitialData = {
+          uid: uid,
+          username: username,
+          email: email,
+          role: 'customer',
+          created_at: timestamp,
+          updated_at: timestamp,
+        };
+
+        db.collection('users')
+          .doc(uid)
+          .set(userInitialData)
+          .then(() => {
+            dispatch(push('/'));
+          });
+      }
+    });
   };
 };
